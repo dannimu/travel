d8c5ce1b8338a445a3f70e05082cf5ceeaf2d643
yinan <yinan@email.com>
2021-12-10 14:02:49 +0400

    travel

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+import vuetify from './plugins/vuetify'
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
 new Vue({
+  router,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
